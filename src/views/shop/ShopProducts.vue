8fbfcc17a1f061c18fbcbb816b840177417c1902
ispan yon <pzbks22182@gmail.com>
2025-03-25 16:48:27 +0800

    批量更新完成，新增商品 製作中

--- src/views/shop/ShopProducts.vue
+++ src/views/shop/ShopProducts.vue
@@ -24,7 +24,7 @@
                                 </td>
                                 <td>
                                     <select v-model="selectedCategory" class="form-select">
-                                        <option value="所有商品">所有商品</option>
+                                        <option value="">所有商品</option>
                                         <option value="食品保健">食品保健</option>
                                         <option value="日常用品">日常用品</option>
                                         <option value="服飾">服飾</option>
@@ -49,9 +49,9 @@
                                 <td>
                                     <select v-model="stockQuantityLessThan" class="form-select">
                                         <option value="">全部</option>
-                                        <option value="20">&lt; 20</option>
-                                        <option value="50">&lt; 50</option>
-                                        <option value="100">&lt; 100</option>
+                                        <option value="20">&lt;= 20</option>
+                                        <option value="50">&lt;= 50</option>
+                                        <option value="100">&lt;= 100</option>
                                     </select>
                                 </td>
                                 <td>
@@ -59,8 +59,8 @@
                                     <span class="to-text mx-2">到</span>
                                     <input v-model="endDate" type="date" class="form-control d-inline-block w-auto">
                                 </td>
-                                <div class="d-flex ">
-                                    <button @click="" class="btn btn-primary">篩選</button>
+                                <div class="d-flex">
+                                    <button @click="onChangePage" class="btn btn-primary">篩選</button>
                                     <button @click="clearFilters" class="btn btn-secondary ms-2">清除</button>
                                 </div>
                             </tr>
@@ -76,15 +76,14 @@
                         <option value=1>上架</option>
                         <option value=0>下架</option>
                     </select>
-                    <button @click="batchUpdateOrders" class="btn btn-warning"
+                    <button @click="batchUpdateProducts" class="btn btn-warning"
                         style="margin-left: 10px; margin-right: 10px;">批量更新</button>
-
-
-                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addOrderModal">新增商品</button>
+                    <button class="btn btn-primary" @click="addProduct">新增商品</button>
+                    <span class="text-danger ms-3">{{ batchUpdateMsg }}</span>
                 </div>
 
 
-                <!-- 新增訂單 Modal -->
+                <!-- 新增商品 Modal -->
                 <!-- <div class="modal fade" id="addOrderModal" tabindex="-1" aria-labelledby="addOrderModalLabel"
                     aria-hidden="true">
                     <div class="modal-dialog modal-lg">
@@ -209,6 +208,7 @@
                         </div>
                     </div>
                 </div> -->
+
             </div>
 
 
@@ -240,12 +240,14 @@
                             <!-- <i
                                 :class="sortDirection === 'orderDetail.memberId' ? (isAscending ? 'bi bi-caret-up-fill' : 'bi bi-caret-down-fill') : ''"></i> -->
                         </th>
+                        <th>建立日期</th>
                         <th>操作</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="product in productList" :key="product.id">
-                        <td><input type="checkbox" v-model="selectedProducts" :value="product.id"></td>
+                        <td><input type="checkbox" v-model="selectedProducts" :value="product.id"
+                                @change="onChangeClickBox"></td>
                         <td>{{ product.status ? "上架" : "下架" }}</td>
                         <td>{{ product.productDetail.name }}</td>
                         <td>{{ product.productSize?.name ?? "-" }}</td>
@@ -253,6 +255,7 @@
                         <td>$ {{ product.unitPrice }}</td>
                         <td>$ {{ product.discountPrice ? product.discountPrice : "-" }}</td>
                         <td>{{ product.stockQuantity }}</td>
+                        <td>{{ dayjs(product.createdTime).format("YYYY-MM-DD") }}</td>
                         <td>
                             <button class="btn btn-sm btn-secondary" @click="viewProduct(product.id)">
                                 <i class="bi bi-eye"></i>
@@ -268,9 +271,8 @@
                 </tbody>
             </table>
 
-            <!-- 顯示訂單詳情 Modal -->
-            <!-- <OrderDetailModal :isEditing="isEditing" :order-detail="orderDetail" :cities="cities"
-                @edit-order="editOrder" @save-order="saveOrder" /> -->
+            <!-- 新增商品 Modal -->
+            <AddProductModal ref="addProductModalRef" />
 
             <!-- 分頁 -->
             <div class="container" v-if="total > 0">
@@ -286,24 +288,34 @@
 
 <script setup>
 import { ref, onMounted, watch, nextTick } from 'vue';
-import Paginate from 'vuejs-paginate-next';
 import axios from 'axios';
+import Paginate from 'vuejs-paginate-next';
+import dayjs from 'dayjs';
+import Swal from 'sweetalert2';
+
+import AddProductModal from '@/components/shop/AddProductModal.vue';
 
 const PATH = import.meta.env.VITE_API_URL;
 
 // ===================== 初始載入 =====================
 const productList = ref([]);    // 加載商品
-const selectedProducts = ref([]);    // 勾選的商品
 
 const searchProductKeyword = ref("");   // 關鍵字搜尋
-const selectedCategory = ref("所有商品");   // 商品分類
+const selectedCategory = ref("");   // 商品分類
 const productStatus = ref("");  // 上架狀態
 const isProductDiscount = ref("");  // 特價狀態
 const stockQuantityLessThan = ref("");  // 庫存狀態
 const startDate = ref('');
 const endDate = ref('');
 
+//  批量更新 
+const selectedProducts = ref([]);    // 勾選的商品
+const selectAll = ref(false);
+const batchStatus = ref("");
+const batchUpdateMsg = ref("");
 
+// 新增商品 Modal
+const addProductModalRef = ref(null);
 
 // 分頁
 const currentPage = ref(1); // 目前在第幾頁
@@ -317,16 +329,22 @@ const lastPageRows = ref(0); // 最後一頁有幾筆
 
 onMounted(async () => {
     onChangePage();
+
 });
 
 // 監聽所有過濾選項
-watch([searchProductKeyword, selectedCategory, productStatus, isProductDiscount], () => {
+watch([searchProductKeyword, selectedCategory, productStatus, isProductDiscount, stockQuantityLessThan, startDate, endDate], () => {
     onChangePage();
 });
 
 // #endregion =================================================
 
 // #region Event function =================================================
+
+// test
+function onChangeClickBox() {
+    // console.log(selectedProducts.value);
+}
 
 // 分頁切換 => 加載商品
 function onChangePage(page) {
@@ -342,10 +360,15 @@ function onChangePage(page) {
         "start": start.value,
         "rows": rows.value,
         "keyword": searchProductKeyword.value ? searchProductKeyword.value : null,
-        "category": selectedCategory.value,
+        "category": selectedCategory.value ? selectedCategory.value : null,
         "status": productStatus.value ? productStatus.value : null,
         "isProductDiscount": isProductDiscount.value ? isProductDiscount.value : null,
+        "stockQuantityLessThan": stockQuantityLessThan.value ? stockQuantityLessThan.value : null,
+        "startDate": startDate.value ? startDate.value : null,
+        "endDate": endDate.value ? endDate.value : null,
     }
+
+    console.log(filterData);
 
     getProducts(filterData);
 
@@ -363,7 +386,7 @@ async function getProducts(filterData) {
         params: filterData
     })
         .then(response => {
-            console.log(response.data);
+            // console.log(response.data);
             productList.value = response.data.productList;
 
             // 分頁
@@ -376,6 +399,72 @@ async function getProducts(filterData) {
 
 }
 
+// 清除篩選
+const clearFilters = () => {
+
+    searchProductKeyword.value = '';
+    selectedCategory.value = '';
+    productStatus.value = '';
+    isProductDiscount.value = '';
+    stockQuantityLessThan.value = '';
+    startDate.value = '';
+    endDate.value = '';
+
+    batchUpdateMsg.value = '';
+
+    onChangePage();
+
+};
+
+// 切換全選
+const toggleAll = () => {
+    if (selectAll.value) {
+        selectedProducts.value = productList.value.map(product => product.id);
+    } else {
+        selectedProducts.value = [];
+    }
+};
+
+// 批量更新
+const batchUpdateProducts = async () => {
+    if (!batchStatus.value) {
+        batchUpdateMsg.value = "請選擇狀態";
+        return;
+    }
+
+    const updatedProductsData = {
+        productIds: selectedProducts.value.join(','),
+        batchStatus: batchStatus.value
+    };
+
+    axios({
+        method: 'put',
+        url: `${PATH}/manage/shop/products/api/updateProductsStatus`,
+        params: updatedProductsData
+
+    })
+        .then(response => {
+            console.log(response.data);
+            batchUpdateMsg.value = "批量更新成功";
+            selectedProducts.value = [];
+            selectAll.value = false;
+
+            onChangePage(currentPage.value);
+
+        })
+        .catch(error => {
+            console.log(error);
+            batchUpdateMsg.value = "批量更新失敗";
+        });
+
+};
+
+
+// 新增商品
+function addProduct() {
+    addProductModalRef.value.showModal();
+
+}
 
 // ===============sortby===========
 const sortDirection = ref('');
