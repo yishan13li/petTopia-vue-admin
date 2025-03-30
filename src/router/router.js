5f84e8e1f362e71b8b8d6081f511e79442af82b6
Melody <melodyshan0504@gmail.com>
2025-03-31 01:33:20 +0800

    新增商品評論管理(未完成)

--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';  // 引入 Home.vue
 import Login from '@/views/Login.vue';
 import ShopOrders from '@/views/shop/ShopOrders.vue';
-
+import ShopProductReviews from '@/views/shop/ShopProductReviews.vue';
 
 const routes = [
     {
@@ -20,6 +20,11 @@ const routes = [
         path: '/manage/shop/orders',
         name: 'ShopOrders',
         component: ShopOrders
+    },
+    {
+        path: '/manage/shop/productReviews',
+        name: 'ShopProductReviews',
+        component: ShopProductReviews
     },
 
     // 可以根據需要添加更多路由
